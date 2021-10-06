d3d0c2673f547b084addd3b75dc726201657a89e
Yulin2004 <yroozemond@gmail.com>
2021-10-06 11:39:49 +0200

    'nogmaals'

---
+++ guess.py
@@ -0,0 +1,86 @@
+import random
+
+#SETUP
+
+guess = 0
+score = 0
+
+print('wat is je naam? ')
+myName = input()
+
+number = random.randint(1, 10)
+print ('oke,' + myName+ ',raad het getal tussen 1 en 20.')
+
+#raden welk nummer het is.
+
+while guess <= 8:
+     print('')
+     print('raad het.')
+
+     guessIt = input()
+     guessIt = int(guessIt)
+
+     guess+guess+8
+
+if guessIt < number:
+         print('te laag...')
+
+if guessIt > number:
+        print('te hoog...')
+
+if guessIt > number:
+
+    if number <= 20:
+        print('bijna')
+if guess == number:
+    
+    if guessIt == number:
+        guess = str(guess)
+    print('goed geraden...\nyou guess ' + guess + '... took u a while...\n')
+
+if guessIt != number:
+    number = str(number)
+    print('verkeerd... the number was ' + number)
+
+if number == True:
+    score += 1
+    
+print ('want to play again? y/n ')
+
+if "y":
+    print ('okay')
+    while guess <= 8:
+         print('')
+    print('raad het.')
+
+    guessIt = input()
+    guessIt = int(guessIt)
+
+    guess+guess+8
+
+    if guessIt < number:
+         print('te laag...')
+
+    if guessIt > number:
+        print('te hoog...')
+
+    if guessIt > number:
+
+        if number <= 20:
+            print('bijna')
+    if guess == number:
+    
+        if guessIt == number:
+            guess = str(guess)
+    print('goed geraden...\nyou guess ' + guess + '... took u a while...\n')
+
+    if guessIt != number:
+        number = str(number)
+    print('verkeerd... the number was ' + number)
+
+    if number == True:
+        score += 1
+    else:
+        print ('thats okay.. :(')
+        print (score)
+        quit
